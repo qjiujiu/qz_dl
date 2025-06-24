ec74c0d8fb25ceba83eac5f66cc6431c39aa833c
QJM <1006485793@qq.com>
2025-06-24 23:30:38 +0800

    feat: 更新参数类，新增测试脚本

--- config/params_parser.py
+++ config/params_parser.py
@@ -5,37 +5,39 @@ from typing import Optional
 from dataclasses import dataclass, field
 import torch
 
+
+
 @dataclass
 class CommonCfgParams:
-    batch_size: int             # 批处理大小
-    epochs: int                 # 训练轮数
-    lr: float                   # 学习率
-    device: str                 # 运行设备
-    checkpoint_path: str        # 模型保存路径
+    batch_size: int = 32                   # 批处理大小
+    epochs: int = 10                       # 训练轮数
+    lr: float = 0.01                       # 学习率
+    dropout_prob: float = 0.5              # 随机失活概率
+    device: str = "cuda"                   # 运行设备
+    checkpoint_path: Optional[str] = None  # 模型保存路径，使用 Optional 和 None 表示可选字段
     
     def __post_init__(self):
+        # 自动根据设备环境选择
         if torch.cuda.is_available():
             self.device = "cuda"
         else:
             self.device = "cpu"
-   
 
 
 @dataclass
 class CvCfgParams(CommonCfgParams):
     pass
 
+
 @dataclass
 class NlpCfgParams(CommonCfgParams):
-    vocab_size: int             # 词表大小
-    embedding_dim: int          # 词向量维度
-    hidden_dim: int             # 隐藏层维度
-    output_dim: int             # 输出类别数
-    max_len: int                # 最大序列长度
-   
+    vocab_size: Optional[int] = None             # 词表大小
+    embedding_dim: Optional[int] = None          # 词向量维度
+    hidden_dim: Optional[int] = None             # 隐藏层维度
+    output_dim: Optional[int] = None             # 输出类别数
+    max_len: Optional[int] = None                # 最大序列长度
   
-    only_embed: bool = False    # 直接传入embedding 向量进行训练，而不传入文件索引
+    only_embed: bool = False                     # 直接传入embedding 向量进行训练，而不传入文件索引
     
-
     def __post_init__(self):
         pass
---
+++ testcases/x-测试命令行参数转为参数类.py
@@ -0,0 +1,19 @@
+import os, sys
+sys.path.append("./")
+sys.path.append("../")
+
+from config.parser import ArgsParser
+
+
+
+if __name__ == '__main__':
+    args = ArgsParser().create_cv_config()
+    print(args)
+
+    args = ArgsParser().create_nlp_config()
+    print(args)
+
+    
+
+    
+
