93eb43d00a421931d33aee6ac1cc6ba8907c7a0a
qjiujiu <1006485793@qq.com>
2025-07-08 09:39:02 +0800

    feat: 允许通过超参数控制是否直接传入embedding

--- train_lstm.py
+++ train_lstm.py
@@ -3,38 +3,51 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 
-from utils.models import pick_model
-
 from config.datasets.datasrc.text_datasrc import TextDataSrc
 from config.params_parser.parser import ArgsParser
 from config.logger import logger
 
-from utils import random_seed
+from utils.models import (
+    pick_model, 
+    pick_embedding_encoder
+)
+
 
 logger.is_debug(True)
 
-# 文本输入
-# python train_lstm.py --model lstm  --batch-size 8 --epochs 30 --lr 0.001 --dropout-prob 0.5  --embedding-dim 128 --hidden-dim 256 --output-dim 8  --max-len 200  --vocab-size 278
-# Embedding 输入
-# python train_lstm.py --only-embed True --model lstm  --batch-size 8 --epochs 30 --lr 0.001 --dropout-prob 0.5  --embedding-dim 128 --hidden-dim 256 --output-dim 8  --max-len 200  --vocab-size 278
+""" 使用说明
+    - 文本输入: 
+        python train_lstm.py --model lstm  -bs 8 -ep 30 --lr 0.001 -eb 128 --hidden-dim 256 --output-dim 8  --max-len 200  --vocab-size 278
+
+    - Embedding 输入
+        python train_lstm.py --only-embed -ec default --model lstm -bs 8 --ep 30 --lr 0.001 -eb 128 --hidden-dim 256 --output-dim 8  --max-len 200  --vocab-size 278
+        python train_lstm.py --only-embed -ec default --model lstm -bs 8 --ep 30 --lr 0.001 -eb 128 --hidden-dim 256 --output-dim 8  --max-len 200  --vocab-size 278 -cp checkpoints/2025-07-08/LSTMTextClassifier/20250708-0840-11838954_weights.pth
+
+    若想载入权重，可添加 --checkpoint-path (缩写 -cp) 参数
+    若想跳过文本格式数据直接传输向量，可添加 --only-embed -ec default
+
+    特别强调，如果直接使用预训练的 model embedding 模块产出的向量训练，必须传入预训练模型的权重路径
+"""
+
+
 
 if __name__ == "__main__":
-    random_seed.set_random_seed(3407)
-
     cfg =  ArgsParser().create_nlp_config()
+    model = pick_model(cfg, cfg.checkpoint_path)
+    logger.debug(
+        f"模型结构: {model}"
+        f"预测头层数: {cfg.L + 1}"
+    )
+
+
     data_resource = TextDataSrc.load_dataset(
         dataset_name="malapi", 
-        batch_size=cfg.batch_size
+        batch_size=cfg.batch_size, 
     )
 
     logger.debug(f"本轮训练的超参数设置: {cfg}")
     logger.debug(f"使用的训练数据规模: {data_resource}")    
 
-    model = pick_model(cfg)
-    
-    logger.debug(f"模型结构: {model}")
-    logger.debug(f"预测头层数: {cfg.L + 1}")
-    
 
     # 定义损失函数和优化器
     criterion = nn.CrossEntropyLoss()
@@ -44,10 +57,19 @@ if __name__ == "__main__":
         .setup_loss(criterion)\
         .setup_optimizer(optimizer)
 
+    
+    # 如果开启向量模式，会通过 encoder 来将索引转为向量，再把向量丢给 model
+    encoder = pick_embedding_encoder(cfg, cfg.load_path)
+    logger.debug(
+        f"是否开启 embedding 模式: {cfg.only_embed}"   # 是否开启向量模式
+        f"当前使用 embedding 模块结构: {encoder}"       # 若不开启默认为空
+    )
+    
     # 最后一轮评估的结果就是测试集上面跑出来的结果
     model.train_multiple_epochs(
         loader = data_resource.train_loader, 
         val_loader = data_resource.test_loader, 
-        epochs = cfg.epochs
+        epochs = cfg.epochs, 
+        encoder = encoder
     )
     
