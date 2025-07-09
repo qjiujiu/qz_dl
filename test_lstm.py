fc4dafeda2b281d5f362804265db32d4924c7e0b
QJM <1006485793@qq.com>
2025-07-09 11:31:05 +0800

    feat：重构测试脚本

--- test_lstm.py
+++ test_lstm.py
@@ -1,57 +1,75 @@
-# test_lstm.py
+# train_lstm.py
 import torch
-from torch.utils.data import DataLoader
-from models.nlp.lstm_text_classifier import LSTMTextClassifier
-from config.datasets.dataset_instance import mal_api
-from config.logger import logger_initiate
-from tqdm import tqdm
-from utils.get_config import load_config
+import torch.optim as optim
+import torch.nn as nn
 
-# 测试模型
-def test_model(config):
-    # 设置超参数
-    max_len = config['max_len']
-    batch_size = config['batch_size']
+from config.datasets.datasrc.text_datasrc import TextDataSrc
+from config.params_parser.parser import ArgsParser
+from config.logger import logger
 
-    # 数据加载
-    _, test_dataset, vocab = mal_api.load(config['train_data'], config['train_labels'])
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+from utils.models import (
+    pick_model, 
+    pick_embedding_encoder
+)
 
-    # 创建模型
-    model = LSTMTextClassifier(
-        vocab_size=len(vocab),
-        embedding_dim=config['embedding_dim'],
-        hidden_dim=config['hidden_dim'],
-        output_dim=config['output_dim'],
-        max_len=max_len
-    )
 
-    # 加载训练好的模型权重
-    checkpoint_path = config['checkpoint_path']
-    model.load_state_dict(torch.load(checkpoint_path))
-    model.eval()  # 设置为评估模式
+logger.is_debug(True)
 
-    # 日志记录
-    logger = logger_initiate(log_level=config.get('log_level', 'INFO'), is_console=True, is_file=True, is_colorful=True)
+""" 使用说明
+    - 文本输入: 
+        python test_lstm.py --model lstm  -bs 8 -ep 30 --lr 0.001 -eb 128 --hidden-dim 256 --output-dim 8  --max-len 200  --vocab-size 278 -cp checkpoints/2025-07-09/LSTMTextClassifier/20250709-0954-ff28631f_weights.pth
 
-    # 评估模型
-    correct = 0
-    total = 0
+    - Embedding 输入
+        python train_lstm.py --only-embed -ec default --model lstm -bs 8 -ep 30 --lr 0.001 -eb 128 --hidden-dim 256 --output-dim 8  --max-len 200  --vocab-size 278 -cp checkpoints/2025-07-09/LSTMTextClassifier/20250709-0954-ff28631f_weights.pth
 
-    with torch.no_grad():
-        with tqdm(test_loader, desc="Evaluating", unit="batch") as tepoch:
-            for texts, labels in tepoch:
-                x = model.embed(texts)
-                print(x.shape)
-                outputs = model(x)
-                _, predicted = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-                tepoch.set_postfix(accuracy=correct / total * 100)
+    若想载入权重，可添加 --checkpoint-path (缩写 -cp) 参数
+    若想跳过文本格式数据直接传输向量，可添加 --only-embed -ec default
 
-    accuracy = correct / total
-    logger.info(f"Test Accuracy: {accuracy * 100:.2f}%")
+    特别强调，如果直接使用预训练的 model embedding 模块产出的向量训练，必须传入预训练模型的权重路径
+"""
+
+
 
 if __name__ == "__main__":
-    config = load_config("config/lstm_config.yaml")  # 加载配置
-    test_model(config)      # 测试模型
+    cfg =  ArgsParser().create_nlp_config()
+    model = pick_model(cfg, cfg.checkpoint_path)
+    logger.debug(
+        f"模型结构: {model}"
+        f"预测头层数: {cfg.L + 1}"
+    )
+
+
+    data_resource = TextDataSrc.load_dataset(
+        dataset_name="malapi", 
+        batch_size=cfg.batch_size, 
+    )
+
+    logger.debug(f"本轮训练的超参数设置: {cfg}")
+    logger.debug(f"使用的训练数据规模: {data_resource}")    
+
+
+    # 定义损失函数和优化器
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=cfg.lr)
+
+    model.setup_ctx(cfg)\
+        .setup_loss(criterion)\
+        .setup_optimizer(optimizer)
+
+    
+    # 如果开启向量模式，会通过 encoder 来将索引转为向量，再把向量丢给 model
+    encoder = pick_embedding_encoder(cfg, cfg.load_path)
+    logger.debug(
+        f"是否开启 embedding 模式: {cfg.only_embed}"     # 是否开启向量模式
+        f"当前使用外部 encoder: {encoder}"                # 若不开启默认为空
+    )
+    
+    # 最后一轮评估的结果就是测试集上面跑出来的结果
+    
+    result =  model.evalution(
+        dataloader = data_resource.test_loader, 
+        encoder = encoder
+    )
+
+    logger.debug(result)
+    
