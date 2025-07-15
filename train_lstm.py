514be66cb321bb52186f6331c2031aebad030fcd
qjiujiu <1006485793@qq.com>
2025-07-15 18:42:27 +0800

    docs: 调整注释紧凑度

--- train_lstm.py
+++ train_lstm.py
@@ -31,10 +31,8 @@ logger.is_debug(True)
     
     其它说明: 
         1. 若想载入权重，可添加 --checkpoint-path (缩写 -cp) 参数 
-        
         2. 若想跳过文本格式数据直接传输向量，可添加 --encoder (缩写 -ec) 参数，encoder也可以使用先前模型预训练的 embedding 参数，这种情况必须
            传入预训练模型的权重路径
-
         3. 若想切换数据集 可添加 --dataset 数据集选项: malapi_cleanemb, malapi_fgsmemb, malapi_pgdemb
 
     特别强调，如果直接使用预训练的 model embedding 模块产出的向量训练，必须传入预训练模型的权重路径
@@ -56,7 +54,7 @@ if __name__ == "__main__":
     
 
     # 定义损失函数和优化器
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
     optimizer = optim.Adam(model.parameters(), lr=cfg.lr)
 
     model.setup_ctx(cfg)\
