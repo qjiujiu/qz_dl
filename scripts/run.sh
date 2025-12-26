cd5130254da3dadd1edb3daf164ede732d49ae5a
QJM <1006485793@qq.com>
2025-12-26 11:56:13 +0800

    fix: 修复bash脚本语法错误

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@ conda activate qtorch
 
 # Define models and noise types
 models=("tcn" "lstm" "conv1d" "conv2d")
-noise_types=("ctx" "ctx_sa" "ctx_sape" "ctx_mlp", "ctx_gas")
+noise_types=("ctx" "ctx_sa" "ctx_sape" "ctx_mlp" "ctx_gas")
 
 # Loop over models and noise types to run the training scripts
 for model in "${models[@]}"; do
